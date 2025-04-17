0fe3adadd1e3d91e41251abceb1cd86dc3dd876b
Dakota <dakotabarnes@ucsb.edu>
2025-04-17 13:58:29 -0700

    fix bug for longevity

--- src/civrealm/freeciv/utils/base_action.py
+++ src/civrealm/freeciv/utils/base_action.py
@@ -184,7 +184,7 @@ class ActionList(object):
         act = self.get_action(actor_id, action_id)
         # if not self._can_actor_act(actor_id):
         #     raise Exception('_can_actor_act error')
-        if act.is_action_valid():
+        if act != None and act.is_action_valid():
             act.trigger_action(self.ws_client, **kwargs)
             return True
         print(f"Action {action_id} of {actor_id} is not valid")
